a83a594ecf3893890ad1f2c54b36ecfa03972140
hayter bordon <hayterbordon.d@gmail.com>
2025-05-11 22:38:44 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
-var gulp = require('gulp'); //default
-var sass = require('sass'); // Reemplazado gulp-sass por sass
-var sassCompiler = require('gulp-sass')(sass); // Importante para que funcione correctamente
-var concat = require('gulp-concat'); //compile js plugins into one file
-var concatCss = require('gulp-concat-css'); //compile css plugins into one file
-var watch = require('gulp-watch'); //sass compile to css
+const gulp = require('gulp'); //default
+const sass = require('sass'); // Reemplazado gulp-sass por sass
+const sassCompiler = require('gulp-sass')(sass); // Importante para que funcione correctamente
+const concat = require('gulp-concat'); //compile js plugins into one file
+const concatCss = require('gulp-concat-css'); //compile css plugins into one file
+const watch = require('gulp-watch'); //sass compile to css
 
 // Tarea para compilar SASS
 gulp.task('sass', function () {
     return gulp.src('html/scss/**/*.scss') // Carpeta donde están los archivos .scss
-            .pipe(sassCompiler().on('error', sassCompiler.logError)) // Usar sassCompiler en lugar de gulp-sass
-            .pipe(gulp.dest('html/css/')); // Carpeta de salida para los archivos CSS
+        .pipe(sassCompiler().on('error', sassCompiler.logError)) // Usar sassCompiler en lugar de gulp-sass
+        .pipe(gulp.dest('html/css/')); // Carpeta de salida para los archivos CSS
 });
 
 // Tarea para observar cambios en los archivos SCSS
@@ -33,21 +33,21 @@ gulp.task('concat', function () {
                 'html/bower_components/jarallax/dist/jarallax-video.min.js',
                 'html/js/smooth-scroll.min.js'
             ])
-            .pipe(concat('plugins.js'))
-            .pipe(gulp.dest('html/js/plugins/'));
+        .pipe(concat('plugins.js'))
+        .pipe(gulp.dest('html/js/plugins/'));
 });
 
 // Tarea para concatenar todos los archivos CSS
 gulp.task('concatCss', function () {
     return gulp.src([
-        'html/bower_components/font-awesome/css/font-awesome.min.css',
-        'html/css/animate.css',
-        'html/bower_components/magnific-popup/dist/magnific-popup.css',
-        'html/bower_components/owl.carousel/dist/assets/owl.carousel.min.css',
-        'html/bower_components/owl.carousel/dist/assets/owl.theme.default.min.css',
-        'html/et-line-font/style.css',
-        'html/bootstrap/dist/css/bootstrap.min.css'
-    ])
-            .pipe(concatCss("plugins/plugins.css"))
-            .pipe(gulp.dest('html/css/'));
+            'html/bower_components/font-awesome/css/font-awesome.min.css',
+            'html/css/animate.css',
+            'html/bower_components/magnific-popup/dist/magnific-popup.css',
+            'html/bower_components/owl.carousel/dist/assets/owl.carousel.min.css',
+            'html/bower_components/owl.carousel/dist/assets/owl.theme.default.min.css',
+            'html/et-line-font/style.css',
+            'html/bootstrap/dist/css/bootstrap.min.css'
+        ])
+        .pipe(concatCss("plugins/plugins.css"))
+        .pipe(gulp.dest('html/css/'));
 });

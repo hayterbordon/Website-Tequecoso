652cc65639127e1f5eb8602ae235e83ad683f844
hayter bordon <hayterbordon.d@gmail.com>
2025-05-14 14:29:33 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,59 @@
-var gulp = require('gulp'); //default
-var sass = require('gulp-sass');//sass complie
-var concat = require('gulp-concat'); //compile js plugins into one file
-var concatCss = require('gulp-concat-css');//compile css plugins into one file
-var watch = require('gulp-watch'); //sass compile to css
+const gulp = require('gulp');
+const sass = require('gulp-dart-sass'); // Reemplazo moderno de gulp-sass
+const concat = require('gulp-concat');
+const concatCss = require('gulp-concat-css');
+const watch = require('gulp-watch');
 
-gulp.task('sass', function () {
-    return gulp.src('html/scss/**/*.scss')
-            .pipe(sass().on('error', sass.logError))
-            .pipe(gulp.dest('html/css/'));
-});
-gulp.task('watch', function () {
-    gulp.watch('html/scss/**/*.scss', ['sass']);
-});
+// 1. Compila Sass a CSS
+function compileSass() {
+  return gulp.src('html/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('html/css/'));
+}
 
-gulp.task('concat', function () {
-    return gulp.src(
-            [
-                'html/bower_components/jquery/dist/jquery.min.js',
-                'html/bootstrap/dist/js/bootstrap.min.js',
-                'html/js/jquery.easing.min.js',
-                'html/bower_components/wow/dist/wow.min.js',
-                'html/js/jquery.preloader.min.js',
-                'html/bower_components/magnific-popup/dist/jquery.magnific-popup.min.js',
-                'html/bower_components/jquery-knob/dist/jquery.knob.min.js',
-                'html/bower_components/owl.carousel/dist/owl.carousel.min.js',
-                'html/bower_components/jarallax/dist/jarallax.min.js',
-                'html/bower_components/jarallax/dist/jarallax-video.min.js',
-                'html/js/smooth-scroll.min.js'
-            ])
-            .pipe(concat('plugins.js'))
-            .pipe(gulp.dest('html/js/plugins/'));
-});
+// 2. Concatena archivos JS
+function concatJs() {
+  return gulp.src([
+    'html/bower_components/jquery/dist/jquery.min.js',
+    'html/bootstrap/dist/js/bootstrap.min.js',
+    'html/js/jquery.easing.min.js',
+    'html/bower_components/wow/dist/wow.min.js',
+    'html/js/jquery.preloader.min.js',
+    'html/bower_components/magnific-popup/dist/jquery.magnific-popup.min.js',
+    'html/bower_components/jquery-knob/dist/jquery.knob.min.js',
+    'html/bower_components/owl.carousel/dist/owl.carousel.min.js',
+    'html/bower_components/jarallax/dist/jarallax.min.js',
+    'html/bower_components/jarallax/dist/jarallax-video.min.js',
+    'html/js/smooth-scroll.min.js'
+  ])
+  .pipe(concat('plugins.js'))
+  .pipe(gulp.dest('html/js/plugins/'));
+}
 
-gulp.task('concatCss', function () {
-    return gulp.src([
-        'html/bower_components/font-awesome/css/font-awesome.min.css',
-        'html/css/animate.css',
-        'html/bower_components/magnific-popup/dist/magnific-popup.css',
-        'html/bower_components/owl.carousel/dist/assets/owl.carousel.min.css',
-        'html/bower_components/owl.carousel/dist/assets/owl.theme.default.min.css',
-        'html/et-line-font/style.css',
-        'html/bootstrap/dist/css/bootstrap.min.css'
-    ])
-            .pipe(concatCss("plugins/plugins.css"))
-            .pipe(gulp.dest('html/css/'));
-});
+// 3. Concatena archivos CSS
+function concatCssPlugins() {
+  return gulp.src([
+    'html/bower_components/font-awesome/css/font-awesome.min.css',
+    'html/css/animate.css',
+    'html/bower_components/magnific-popup/dist/magnific-popup.css',
+    'html/bower_components/owl.carousel/dist/assets/owl.carousel.min.css',
+    'html/bower_components/owl.carousel/dist/assets/owl.theme.default.min.css',
+    'html/et-line-font/style.css',
+    'html/bootstrap/dist/css/bootstrap.min.css'
+  ])
+  .pipe(concatCss("plugins.css"))
+  .pipe(gulp.dest('html/css/plugins/')); // Cambiado a una subcarpeta más organizada
+}
+
+// 4. Vigila cambios en archivos SCSS
+function watchFiles() {
+  gulp.watch('html/scss/**/*.scss', compileSass);
+}
+
+// Tareas públicas (para ejecutar desde la terminal)
+exports.sass = compileSass;
+exports.concat = concatJs;
+exports.concatCss = concatCssPlugins;
+exports.watch = watchFiles;
+exports.build = gulp.series(compileSass, concatJs, concatCssPlugins); // Tarea principal
+exports.default = exports.build; // Por si ejecutas solo "gulp"

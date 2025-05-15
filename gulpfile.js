e2985510e2a8321529acc04ea8614d1983c9bd6b
hayter bordon <hayterbordon.d@gmail.com>
2025-05-15 17:12:06 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@ const sassCompiler = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const concatCss = require('gulp-concat-css');
 
-// Compila solo el archivo principal SCSS para evitar múltiples archivos
+// Compila solo el archivo principal SCSS para generar style-saas-landing.css en /css
 function compileSass() {
-  return gulp.src('scss/style-saas-landing.scss')  // apunta solo al principal
+  return gulp.src('scss/style-saas-landing.scss') // Archivo principal
     .pipe(sassCompiler({
-      includePaths: ['scss', 'scss-saas-software'] // para importar parciales sin rutas largas
+      includePaths: ['scss', 'scss-saas-software'] // Para importar parciales sin rutas largas
     }).on('error', sassCompiler.logError))
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('css/')); // Carpeta destino
 }
 
-// Concatena JS de plugins externos
+// Concatena JS de plugins externos en js/plugins/plugins.js
 function concatJs() {
   return gulp.src([
     'bower_components/jquery/dist/jquery.min.js',
@@ -31,22 +31,22 @@ function concatJs() {
   .pipe(gulp.dest('js/plugins/'));
 }
 
-// Concatena CSS de plugins externos
+// Concatena CSS de plugins externos en css/plugins/plugins.css
 function concatCssPlugins() {
   return gulp.src([
     'bower_components/font-awesome/css/font-awesome.min.css',
-    'css/animate.css',  // tu CSS custom animaciones, asegúrate que exista
+    'css/animate.css', // Asegúrate que exista
     'bower_components/magnific-popup/dist/magnific-popup.css',
     'bower_components/owl.carousel/dist/assets/owl.carousel.min.css',
     'bower_components/owl.carousel/dist/assets/owl.theme.default.min.css',
-    // 'et-line-font/style.css', // Comentado porque no existe en tu proyecto
+    // 'et-line-font/style.css', // Comentado porque no existe
     'bootstrap/dist/css/bootstrap.min.css'
   ], { allowEmpty: true })
   .pipe(concatCss('plugins.css'))
   .pipe(gulp.dest('css/plugins/'));
 }
 
-// Vigila cambios en SCSS y JS
+// Vigila cambios en SCSS y JS para recompilar automáticamente
 function watchFiles() {
   gulp.watch('scss/**/*.scss', compileSass);
   gulp.watch('js/**/*.js', concatJs);
@@ -56,5 +56,7 @@ exports.sass = compileSass;
 exports.concatJs = concatJs;
 exports.concatCss = concatCssPlugins;
 exports.watch = watchFiles;
+
+// Build completo: compila sass y concatena JS y CSS plugins
 exports.build = gulp.series(compileSass, concatJs, concatCssPlugins);
 exports.default = exports.build;
